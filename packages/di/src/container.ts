bea6767b178af2fb66408291aa60b9259931dda0
dengyc <dengyc2013@gmail.com>
2020-08-12 18:50:55 +0800

    use Map

--- packages/di/src/container.ts
+++ packages/di/src/container.ts
@@ -1,31 +1,27 @@
 import { Binding } from './binding';
 
 export default class Container {
-    private __all_binding__: { [name: string]: Binding<any> } = {};
+    private m = new Map<string, Binding<any>>();
 
     getNames() {
-        return Object.getOwnPropertyNames(this.__all_binding__);
+        return this.m.keys();
     }
 
     bind<T>(name): Binding<T> {
-        if (!this.__all_binding__[name]) {
+        if (!this.m.has(name)) {
             const __def__ = new Binding(name, this);
-            this.__all_binding__[name] = __def__;
+            this.m.set(name, __def__);
         }
-        return this.__all_binding__[name];
+        return this.m.get(name);
     }
 
     has(name) {
-        if (this.__all_binding__[name]) {
-            return true;
-        }
-        return false;
+        return this.m.has(name);
     }
 
     get<T>(name): Binding<T> | null {
-        const binding: Binding<T> = this.__all_binding__[name];
-        if (binding) {
-            return binding;
+        if (this.has(name)) {
+            return this.m.get(name);
         }
         return null;
     }
