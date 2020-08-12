34066843fc0f896b360d7511e21f69d55d03baee
dengyc <dengyc2013@gmail.com>
2020-08-12 18:09:59 +0800

    import events

--- packages/events/src/index.ts
+++ packages/events/src/index.ts
@@ -1,110 +1,19 @@
-import { Subscription } from './subscription';
+import E from 'events';
 
-const eventSplitter = /\s+/;
+export let eventCenter = new E.EventEmitter();
 
-export interface IEvents {
-    subcount: number;
-    on?: (events) => Subscription;
-    trigger?: (events, ...rest) => any;
-    addListener?: (events, callback, context?) => Events;
-    removeListener?: (events, callback, content?) => Events;
-    proxy?: (events) => (arg) => any;
-}
+const maps = new Map<string, E.EventEmitter>();
 
-export class Events implements IEvents {
-    private __callbacks__;
-
-    subcount = 0;
-    on(events): Subscription {
-        return new Subscription(this, events);
-    }
-
-    addListener(events, callback, context?) {
-        const calls = this.__callbacks__ || (this.__callbacks__ = {});
-        let event, list;
-        events = events.split(eventSplitter);
-
-        while ((event = events.shift())) {
-            list = calls[event] || (calls[event] = []);
-            list.push(callback, context);
-            this.subcount += 1;
+export let events = {
+    eventCenter,
+    scope(scopeName?: string) {
+        if (!scopeName) {
+            return eventCenter;
         }
-        return this;
-    }
-
-    removeListener(events, callback?, context?) {
-        let event, calls, list, i;
-        if (!(calls = this.__callbacks__)) {
-            return this;
+        if (!maps.has(scopeName)) {
+            maps.set(scopeName, new E.EventEmitter());
         }
-        if (!(events || callback || context)) {
-            delete this.__callbacks__;
-            this.subcount = 0;
-            return this;
-        }
-        events = events ? events.split(eventSplitter) : Object.keys(calls);
-        while ((event = events.shift())) {
-            if (!(list = calls[event]) || !(callback || context)) {
-                delete calls[event];
-                continue;
-            }
-
-            for (i = list.length - 2; i >= 0; i -= 2) {
-                if (!((callback && list[i] !== callback) || (context && list[i + 1] !== context))) {
-                    list.splice(i, 2);
-                    this.subcount -= 1;
-                }
-            }
-        }
-
-        return this;
-    }
-
-    trigger(events, ...rest) {
-        let event, calls, list, i, length, args, all;
-        if (!(calls = this.__callbacks__)) {
-            return this;
-        }
-        events = events.split(eventSplitter);
-        while ((event = events.shift())) {
-            if ((all = calls.all)) {
-                all = all.slice();
-            }
-            if ((list = calls[event])) {
-                list = list.slice();
-            }
-            if (list) {
-                for (i = 0, length = list.length; i < length; i += 2) {
-                    list[i].apply(list[i + 1] || this, rest);
-                }
-            }
-            if (all) {
-                args = [event].concat(rest);
-                for (i = 0, length = all.length; i < length; i += 2) {
-                    all[i].apply(all[i + 1] || this, args);
-                }
-            }
-        }
-
-        return this;
-    }
-
-    proxy(events) {
-        return (...args) => {
-            this.trigger(events, ...args);
-        };
-    }
-
-    static include(targetObject) {
-        targetObject.on = Events.prototype.on;
-        targetObject.addListener = Events.prototype.addListener;
-        targetObject.removeListener = Events.prototype.removeListener;
-        targetObject.trigger = Events.prototype.trigger;
-        targetObject.proxy = Events.prototype.proxy;
-    }
-}
-
-export default {
-    Subscription,
-    Events,
+        return maps.get(scopeName);
+    },
 };
+export default events;
