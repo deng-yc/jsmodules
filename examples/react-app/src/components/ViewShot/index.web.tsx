2326ed8b543cd36371b877c0700731d2adbbd965
dengyc <dengyc2013@gmail.com>
2020-08-31 18:17:06 +0800

    add ViewShot

---
+++ examples/react-app/src/components/ViewShot/index.web.tsx
@@ -0,0 +1,25 @@
+import html2canvas from 'html2canvas';
+import React, { useImperativeHandle, useRef } from 'react';
+
+interface IViewShotProps {
+    children?: any;
+}
+
+export const ViewShot = (props: IViewShotProps, ref: any) => {
+    const viewRef = useRef<HTMLDivElement>(null);
+
+    useImperativeHandle(ref, () => {
+        return {
+            capture() {
+                if (viewRef.current) {
+                    return html2canvas(viewRef.current);
+                }
+                return Promise.resolve(null);
+            },
+        };
+    });
+
+    return <div ref={viewRef}>{props.children}</div>;
+};
+
+export default React.forwardRef<{ capture: () => Promise<any> }>(ViewShot);
